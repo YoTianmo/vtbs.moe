60e32c9e41d5283789206cb4cdd2c6b3fbdfa4b1
nanashi <example@example.com>
2020-05-30 15:11:15 +0000

    deploy: f821c04c98b3b6dfe43af1b1dbcb636a8a1f342c

---
+++ precache-manifest.3ba14859ec2182b470593b1badf56b6e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74499832c7e03c1aec45",
+    "url": "/css/app.e8107018.css"
+  },
+  {
+    "revision": "291e51f9f81620eb20a9",
+    "url": "/css/chunk-vendors.1c32ec0d.css"
+  },
+  {
+    "revision": "6377146ade7ed8fcfd90",
+    "url": "/css/detail.d06e8f94.css"
+  },
+  {
+    "revision": "cdc9559eff0d151ea6a9",
+    "url": "/css/tietie.e26dac0b.css"
+  },
+  {
+    "revision": "b5a47515190770e6da3ee730bfb925e2",
+    "url": "/f3366368e4dbbc0f7c0c.worker.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "e630a67595b4b1a45cf3358bbdc4e299",
+    "url": "/img/0.e630a675.png"
+  },
+  {
+    "revision": "18da49440c1acb0ac12d2e845a65ddb2",
+    "url": "/img/1.18da4944.png"
+  },
+  {
+    "revision": "f845ac475216c901febb31e7cadf8137",
+    "url": "/img/2.f845ac47.png"
+  },
+  {
+    "revision": "d6f16503e55c4cab1941e6c9ac2a6b66",
+    "url": "/img/android-chrome-192x192.d6f16503.png"
+  },
+  {
+    "revision": "1e01a2c08a8cd1cd78d95d3fb5281cf8",
+    "url": "/img/face.1e01a2c0.jpg"
+  },
+  {
+    "revision": "caaadb8de300fa58c900b1d0afbfe41d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74499832c7e03c1aec45",
+    "url": "/js/app.27836ed1.js"
+  },
+  {
+    "revision": "291e51f9f81620eb20a9",
+    "url": "/js/chunk-vendors.af3cffb1.js"
+  },
+  {
+    "revision": "6377146ade7ed8fcfd90",
+    "url": "/js/detail.fdeb090e.js"
+  },
+  {
+    "revision": "1cad75b0ce3ef11dbd2b",
+    "url": "/js/detail~macro.3f6d0879.js"
+  },
+  {
+    "revision": "214a168f664ef9d15652",
+    "url": "/js/dev.27f4e9e9.js"
+  },
+  {
+    "revision": "f7472f58261a899bba30",
+    "url": "/js/macro.061ca6f0.js"
+  },
+  {
+    "revision": "cdc9559eff0d151ea6a9",
+    "url": "/js/tietie.755827f4.js"
+  },
+  {
+    "revision": "45fabf1020378b1dbd967d04adc9a85a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
